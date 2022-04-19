313408b95a3883b221230b26a41a5be93d877079
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-04-19 21:25:54 +0530

    TypeScript in 2mins init setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+function hello(world) {
+    if (world === void 0) { world = 'world'; }
+    return "Hello ".concat(world, "! ");
+}
+exports.hello = hello;
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+
+export function hello(world: string = 'world'): string {
+  return `Hello ${world}! `;
+}
+
+export interface User {
+  id: string;
+  email: string;
+  displayName: string;
+  photoURL?: string;
+}
+
+let currentUser: User = {
+  id: '123',
+  email: 'kumargandhi30@gmail.com',
+  displayName: 'Kumar Gandhi'
+};
+
+currentUser.
